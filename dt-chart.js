f97ade8e0b6259c31dc2e005ad3a5fd80251502d
cpin <cpinfold@fastmail.fm>
2017-01-24 21:07:43 +0000

    Move sections around in preparation for split

--- dt-chart.js
+++ dt-chart.js
@@ -2,74 +2,10 @@
 
 'use strict';
 
-var DataTable = $.fn.dataTable;
-DataTable.chart = {};
-DataTable.chart.version = '0.1.0';
+/*
+ * Chart.js specific
+ */
 
-var collate = function(data) {
-    var counts = {}, order = [];
-    for (var i = 0; i < data.length; i++) {
-        if (counts.hasOwnProperty(data[i])) {
-            counts[data[i]] += 1;
-        } else {
-            order.push(data[i]);
-            counts[data[i]] = 1;
-        }
-    }
-
-    return {order:order, data:counts};
-};
-
-var makeColumnDataset = function(dt, columnIndex) {
-    var original = dt.column(columnIndex).data();
-    var all = collate(original);
-    var filtered = collate(dt.column(columnIndex, {search:'applied'}).data());
-    var dataset = { keys:[], values:[] };
-    for (var i = 0; i < all.order.length; i++) {
-        var key = all.order[i];
-        dataset.keys.push(key);
-        if (filtered.data.hasOwnProperty(key)) {
-            dataset.values.push(filtered.data[key]);
-        } else {
-            dataset.values.push(0);
-        }
-    }
-
-    return dataset;
-};
-
-DataTable.chart.redraw = function (dt) {
-    for (var i = 0; i < dt._chart.columns.length; i++) {
-        var col = dt._chart.columns[i];
-        var dataset = makeColumnDataset( dt, col.idx );
-        var divElement = $(col.chart);
-        updateChart(divElement, col.sTitle, dataset, dt._chart.meta[col.chart]);
-    }
-};
-
-DataTable.chart.init = function (dt) {
-    var ctx = dt.settings()[0];
-    var columns = ctx.aoColumns.filter(function (x) {
-        return x.hasOwnProperty("chart");
-    });
-
-    dt._chart = {};
-    dt._chart.columns = columns;
-    dt._chart.meta = {};
-    for (var i = 0; i < dt._chart.columns.length; i++) {
-        var col = dt._chart.columns[i];
-        var elem = $(col.chart);
-
-        // Maintain chart meta keyed by selector
-        dt._chart.meta[col.chart] = prepareChartElement(elem);
-    }
-
-    dt.on('draw.dt', function () {
-        DataTable.chart.redraw( dt );
-    });
-};
-
-// TODO: Chart.js specific at present
 var updateChart = function(divElement, title, dataset, meta) {
 
     // Everything in here is chart.js specific, including workarounds...
@@ -131,7 +67,6 @@ var updateChart = function(divElement, title, dataset, meta) {
     meta.chartObj = chart;
 };
 
-// TODO: Chart.js specific at present
 var prepareChartElement = function(elem) {
     // Everything in here is chart.js specific, including workarounds...
 
@@ -143,6 +78,82 @@ var prepareChartElement = function(elem) {
         height: elem.height()
     };
 };
+
+/*
+ * Private functions
+ */
+
+var collate = function(data) {
+    var counts = {}, order = [];
+    for (var i = 0; i < data.length; i++) {
+        if (counts.hasOwnProperty(data[i])) {
+            counts[data[i]] += 1;
+        } else {
+            order.push(data[i]);
+            counts[data[i]] = 1;
+        }
+    }
+
+    return {order:order, data:counts};
+};
+
+var makeColumnDataset = function(dt, columnIndex) {
+    var original = dt.column(columnIndex).data();
+    var all = collate(original);
+    var filtered = collate(dt.column(columnIndex, {search:'applied'}).data());
+    var dataset = { keys:[], values:[] };
+    for (var i = 0; i < all.order.length; i++) {
+        var key = all.order[i];
+        dataset.keys.push(key);
+        if (filtered.data.hasOwnProperty(key)) {
+            dataset.values.push(filtered.data[key]);
+        } else {
+            dataset.values.push(0);
+        }
+    }
+
+    return dataset;
+};
+
+/*
+ * DataTables API
+ */
+
+var DataTable = $.fn.dataTable;
+DataTable.chart = {};
+DataTable.chart.version = '0.1.0';
+
+DataTable.chart.redraw = function (dt) {
+    for (var i = 0; i < dt._chart.columns.length; i++) {
+        var col = dt._chart.columns[i];
+        var dataset = makeColumnDataset( dt, col.idx );
+        var divElement = $(col.chart);
+        updateChart(divElement, col.sTitle, dataset, dt._chart.meta[col.chart]);
+    }
+};
+
+DataTable.chart.init = function (dt) {
+    var ctx = dt.settings()[0];
+    var columns = ctx.aoColumns.filter(function (x) {
+        return x.hasOwnProperty("chart");
+    });
+
+    dt._chart = {};
+    dt._chart.columns = columns;
+    dt._chart.meta = {};
+    for (var i = 0; i < dt._chart.columns.length; i++) {
+        var col = dt._chart.columns[i];
+        var elem = $(col.chart);
+
+        // Maintain chart meta keyed by selector
+        dt._chart.meta[col.chart] = prepareChartElement(elem);
+    }
+
+    dt.on('draw.dt', function () {
+        DataTable.chart.redraw( dt );
+    });
+};
+
 
 $(document).on('preInit.dt.dtChart', function (e, ctx) {
     if ( e.namespace !== 'dt' ) {
